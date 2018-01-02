0c8d641eccdeca9f8875817417c8a6368dac7c6e
alexanderFrgunov <Argunovalvl@gmail.com>
2018-01-02 19:08:45 +0200

    some improvments

--- app/src/main/java/com/argunov/industrialoxigencalculator/Variant2.java
+++ app/src/main/java/com/argunov/industrialoxigencalculator/Variant2.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import static java.lang.String.format;
 
@@ -17,23 +18,47 @@ public class Variant2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_variant2);
     }
+
     public void onCalculateVariant2(View view) {
         double air=getAirFlow();
         double oxygen=getOxygenFlow();
         double oxygenConcentration=CalcOxy.calcEnrichAirOxyConc(air,oxygen,CalcOxy.OXYGEN_IN_AIR_CONC_BY_VOL,CalcOxy.OXYGEN_PURITY);
         printOxygenConcentration(oxygenConcentration,air,oxygen);
      }
+
     private void printOxygenConcentration(double oxygenConcentration, double airFlow , double oxygenFlow){
-        String message=format(R.string.enrich_air_oxy_conc+"%1$.0f"+R.string.air_flow+"%2$.0f"+R.string.oxy_flow+"%3$.0f",oxygenConcentration,airFlow,oxygenFlow);
+        String message=format(getStrFromR(R.string.enrich_air_oxy_conc)+"\n%.0f\n",oxygenConcentration);
+        message+=format(getStrFromR(R.string.air_flow)+"\n%.0f\n",airFlow);
+        message+=format(getStrFromR(R.string.oxy_flow)+"\n%.0f\n",oxygenFlow);
         TextView textView=findViewById(R.id.outputData);
         textView.setText(message);
     }
+
     private double getOxygenFlow() {
         EditText oxygenFlow=findViewById(R.id.inputOxygenFlow);
-        return Double.valueOf(oxygenFlow.getText().toString());
+        return isEmpty(oxygenFlow)?0:Double.valueOf(oxygenFlow.getText().toString());
         }
+
     private double getAirFlow() {
         EditText FurnaceAirFlow=findViewById(R.id.inputBlastFurnaceAirFlow);
-        return Double.valueOf(FurnaceAirFlow.getText().toString());
+        return isEmpty(FurnaceAirFlow)?0:Double.valueOf(FurnaceAirFlow.getText().toString());
     }
+
+    private boolean isEmpty(EditText etText) {
+        if (etText.getText().toString().trim().length() > 0) {
+            return false;
+        } else {
+            dispToast();
+            return true;
+        }
+    }
+
+    private void dispToast() {
+        Toast.makeText(this, getStrFromR(R.string.message), Toast.LENGTH_LONG ).show();
+    }
+
+    protected String getStrFromR(int resName) {
+        return getResources().getString(resName);
+    }
+
 }
